6e92aa66dd4d2a93d80e0b2daa80360256acc2c4
Alex Tay <alextaymx@gmail.com>
2020-10-03 04:37:53 +0800

    [ECOM-17] Added dynamic array fields in createProduct form, added multiple product variation options

--- src/views/base/forms/CreateProductVarForm.js
+++ src/views/base/forms/CreateProductVarForm.js
@@ -6,6 +6,7 @@ import {
   CCol,
   CForm,
   CFormGroup,
+  CHeader,
   CInput,
   CInputGroup,
   // CInputGroupAppend,
@@ -38,9 +39,13 @@ import { startCase, pick, omit } from "lodash";
 //   };
 // };
 
-function CreateProductVarForm({ field, handleFormSubmit, onChange, decrementStep }) {
-  const inputField = omit(field, "retailPrice", "supplyPrice", "resale");
-  const monetaryInputField = pick(field, "retailPrice", "supplyPrice");
+function CreateProductVarForm({
+  fieldArr,
+  handleFormSubmit,
+  productVarOnChange,
+  decrementStep,
+  addProductVar,
+}) {
   // const radioInput = pick(field, "resale");
   return (
     <>
@@ -50,91 +55,117 @@ function CreateProductVarForm({ field, handleFormSubmit, onChange, decrementStep
             <CCardHeader>Create Product Variation</CCardHeader>
             <CCardBody>
               <CForm action="" method="post" onSubmit={handleFormSubmit}>
-                <CFormGroup row className="my-0">
-                  {Object.keys(inputField).map((key, index) => {
-                    // console.log(key, inputField[key], index, typeof key);
-                    const displayName = startCase(key);
-                    return (
-                      <CCol sm="4" key={index}>
-                        <CFormGroup>
-                          <CLabel htmlFor={key}>{displayName}</CLabel>
-                          <CInput
-                            type="text"
-                            id={key}
-                            name={key}
-                            // autoComplete="on"
-                            placeholder={`Enter ${displayName}`}
-                            value={inputField[key]}
-                            onChange={onChange}
-                            required
-                          />
-                        </CFormGroup>
-                      </CCol>
-                    );
-                  })}
-                  {Object.keys(monetaryInputField).map((key, index) => {
-                    // console.log(key, inputField[key], index, typeof key);
-                    const displayName = startCase(key);
-                    return (
-                      <CCol sm="4" key={index}>
-                        <CFormGroup>
-                          <CLabel htmlFor={key}>{displayName}</CLabel>
-                          <CInputGroup>
-                            <CInputGroupPrepend>
-                              <CInputGroupText>RM</CInputGroupText>
-                            </CInputGroupPrepend>
-                            <CInput
-                              type="text"
-                              id={key}
-                              name={key}
-                              // autoComplete="on"
-                              placeholder={`Enter ${displayName}`}
-                              value={monetaryInputField[key]}
-                              onChange={onChange}
-                              required
-                            />
-                          </CInputGroup>
-                        </CFormGroup>
-                      </CCol>
-                    );
-                  })}
+                {fieldArr.map((productVar, arrIndex) => {
+                  const inputField = omit(
+                    productVar,
+                    "retailPrice",
+                    "supplyPrice",
+                    "resale"
+                  );
+                  const monetaryInputField = pick(
+                    productVar,
+                    "retailPrice",
+                    "supplyPrice"
+                  );
+                  return (
+                    <CCard accentColor="info" key={arrIndex}>
+                      <CCardHeader>{`Product Variation ${arrIndex + 1}`}</CCardHeader>
+                      <CCardBody>
+                        <CFormGroup row className="my-0">
+                          {Object.keys(inputField).map((key, index) => {
+                            // console.log(key, inputField[key], index, typeof key);
+                            const displayName = startCase(key);
+                            return (
+                              <CCol sm="4" key={index}>
+                                <CFormGroup>
+                                  <CLabel htmlFor={key}>{displayName}</CLabel>
+                                  <CInput
+                                    type="text"
+                                    id={key}
+                                    name={key}
+                                    // autoComplete="on"
+                                    placeholder={`Enter ${displayName}`}
+                                    value={inputField[key]}
+                                    onChange={(e) => productVarOnChange(arrIndex, e)}
+                                    required
+                                  />
+                                </CFormGroup>
+                              </CCol>
+                            );
+                          })}
+                          {Object.keys(monetaryInputField).map((key, index) => {
+                            // console.log(key, inputField[key], index, typeof key);
+                            const displayName = startCase(key);
+                            return (
+                              <CCol sm="4" key={index}>
+                                <CFormGroup>
+                                  <CLabel htmlFor={key}>{displayName}</CLabel>
+                                  <CInputGroup>
+                                    <CInputGroupPrepend>
+                                      <CInputGroupText>RM</CInputGroupText>
+                                    </CInputGroupPrepend>
+                                    <CInput
+                                      type="text"
+                                      id={key}
+                                      name={key}
+                                      // autoComplete="on"
+                                      placeholder={`Enter ${displayName}`}
+                                      value={monetaryInputField[key]}
+                                      onChange={(e) => productVarOnChange(arrIndex, e)}
+                                      required
+                                    />
+                                  </CInputGroup>
+                                </CFormGroup>
+                              </CCol>
+                            );
+                          })}
 
-                  <CCol sm="4">
-                    <CFormGroup>
-                      <CLabel>Resale</CLabel>
-                      <CFormGroup variant="custom-radio">
-                        <CInputRadio
-                          custom
-                          id="resale-radio-yes"
-                          name="resale"
-                          value="true"
-                          onChange={onChange}
-                          checked={field.resale === "true"}
-                        />
-                        <CLabel variant="custom-checkbox" htmlFor="resale-radio-yes">
-                          Yes
-                        </CLabel>
-                      </CFormGroup>
-                      <CFormGroup variant="custom-radio">
-                        <CInputRadio
-                          custom
-                          id="resale-radio-no"
-                          name="resale"
-                          value="false"
-                          onChange={onChange}
-                          checked={field.resale === "false"}
-                        />
-                        <CLabel variant="custom-checkbox" htmlFor="resale-radio-no">
-                          No
-                        </CLabel>
-                      </CFormGroup>
-                    </CFormGroup>
-                  </CCol>
-                </CFormGroup>
-
+                          <CCol sm="4">
+                            <CFormGroup>
+                              <CLabel>Resale</CLabel>
+                              <CFormGroup variant="custom-radio">
+                                <CInputRadio
+                                  custom
+                                  id="resale-radio-yes"
+                                  name="resale"
+                                  value="true"
+                                  onChange={(e) => productVarOnChange(arrIndex, e)}
+                                  checked={productVar.resale === "true"}
+                                />
+                                <CLabel
+                                  variant="custom-checkbox"
+                                  htmlFor="resale-radio-yes">
+                                  Yes
+                                </CLabel>
+                              </CFormGroup>
+                              <CFormGroup variant="custom-radio">
+                                <CInputRadio
+                                  custom
+                                  id="resale-radio-no"
+                                  name="resale"
+                                  value="false"
+                                  onChange={(e) => productVarOnChange(arrIndex, e)}
+                                  checked={productVar.resale === "false"}
+                                />
+                                <CLabel
+                                  variant="custom-checkbox"
+                                  htmlFor="resale-radio-no">
+                                  No
+                                </CLabel>
+                              </CFormGroup>
+                            </CFormGroup>
+                          </CCol>
+                        </CFormGroup>
+                      </CCardBody>
+                    </CCard>
+                  );
+                })}
                 <CFormGroup className="form-actions">
                   <CButton onClick={decrementStep} color="secondary">
                     Back
+                  </CButton>
+                  <CButton onClick={addProductVar} color="warning">
+                    Add Variation
                   </CButton>
                   <CButton type="submit" color="primary">
                     Create
